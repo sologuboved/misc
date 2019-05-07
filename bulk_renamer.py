a0535e0527ff72bb11a12160185d8f8f2b2bfaf3
sologuboved <sologuboved@gmail.com>
2019-05-07 17:34:50 +0300

    added file renamer

---
+++ bulk_renamer.py
@@ -0,0 +1,14 @@
+import os
+import re
+
+
+def rename(path, rule, sieve):
+    for filename in filter(sieve, os.listdir(path)):
+        # print(path + filename, path + rule(filename))
+        os.rename(path + filename, path + rule(filename))
+
+
+if __name__ == '__main__':
+    rename('/Users/sologuboved/Werecoder/scholaki/python_cookbook/',
+           lambda x: x[1:],
+           lambda x: re.match(r'^s.+\.py$', x, flags=re.IGNORECASE))
