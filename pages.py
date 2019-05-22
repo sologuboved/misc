f8f88694c481ddb379b8039b040034f27249e05e
sologuboved <sologuboved@gmail.com>
2019-05-22 15:59:41 +0300

    switched to py3

--- pages.py
+++ pages.py
@@ -7,7 +7,7 @@ PAGES = 2
 def count_variants(beg, end, days, pages):
     to_read = end - beg
     for interim in reversed(range(0, days, 5)):
-        print interim, days - interim, float(to_read - interim * pages) / (days - interim)
+        print(interim, days - interim, float(to_read - interim * pages) / (days - interim))
 
 
 if __name__ == '__main__':
--- which_watch.py
+++ which_watch.py
@@ -1,17 +1,13 @@
-def count_time(start, add):
-    next_day = False
-    start_h, start_m, start_s = start
-    add_h, add_m, add_s = add
-    sec = start_s + add_s
-    fin_sec = sec % 60
-    mins = start_m + add_m + sec // 60
-    fin_m = mins % 60
-    fin_hrs = start_h + add_h + mins // 60
-    if fin_hrs >= 24:
-        fin_hrs -= 24
-        next_day = True
-    return next_day, fin_hrs, fin_m, fin_sec
+import time
 
 
-if __name__ == '__main__':
-    print count_time((21, 39, 0), (18, 46, 0))
+def which_watch(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        print()
+        print(func.__name__, 'took', time.strftime("%H:%M:%S", time.gmtime(time.time() - start)))
+        print()
+        return result
+
+    return wrapper
