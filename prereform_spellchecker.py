e322f20d7fcb2b1487d5e77118ff3a9a53cd740c
sologuboved <sologuboved@gmail.com>
2020-08-11 19:22:44 +0300

    excluded abbreviations

--- prereform_spellchecker.py
+++ prereform_spellchecker.py
@@ -39,7 +39,7 @@ class PrereformSpellchecker:
 
 def correct_word(raw_word):
     word = re.sub(r'(и)(?=[аеёиоуыэюя])', i_fixer, raw_word, flags=re.IGNORECASE)
-    if word[-1] in 'бвгджзклмнпрстфхцчщщ':
+    if word.lower() not in ('др', 'проч', 'т', 'д', 'п') and word[-1] in 'бвгджзклмнпрстфхцчшщ':
         word += 'ъ'
     return word
 
