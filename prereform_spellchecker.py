3ceaaf4b02f50049334285aaddc36381e5d73007
sologuboved <sologuboved@gmail.com>
2020-08-11 18:59:41 +0300

    accounted for i & ъ

--- prereform_spellchecker.py
+++ prereform_spellchecker.py
@@ -1,65 +1,54 @@
 import re
-
-# def correct(filename):
-#     with open(filename) as handler:
-#         raw_contents = handler.read()
-#         for consonant in 'бвгджзклмнпрстфхцчшщ':
-#             raw_contents = raw_contents.replace(consonant + " ", consonant + 'ъ' + " ")
-#         for vowel in 'аеёиоуыэюя':
-#             raw_contents = raw_contents.replace('и' + vowel, 'і' + vowel)
-#         for index in range(len(raw_contents)):
-#             pass
-#     with open(filename, 'wt') as handler:
-#         handler.write(raw_contents)
+import json
+from collections import defaultdict
 
 
 class PrereformSpellchecker:
-    def __init__(self, filename):
+    def __init__(self, filename, dump=None):
         self.filename = filename
+        self.dump = dump
         handler = open(self.filename, 'rt')
-        self.contents = re.split(r'\s\s*', handler.read())
+        self.contents = handler.read()
         handler.close()
-        self.report = {'i_count': 0, 'i_fixed': list(), 'ъ_count': 0, 'ъ_replaced': list()}
+        self.report = defaultdict(int)
 
     def launch(self):
-        print(repr(self.contents))
-        # with open(self.filename, 'wt') as handler:
-        #     handler.write(self.raw_contents)
+        self.traverse_words()
+        with open(self.filename, 'wt') as handler:
+            handler.write(self.contents)
+        self.deliver_report()
 
-    def fix_i(self):
-        for index in range(len(self.contents)):
-            char = self.contents[index]
-            if char == 'и':
-                try:
-                    next_char = self.contents[index + 1]
-                except IndexError:
-                    break
-                if next_char in 'аеёиоуыэюя':
-                    self.contents[index] = 'і'
-                    self.report['i_count'] += 1
+    def traverse_words(self):
+        for raw_word in re.findall(r'\b(\S+?)\b|\s', self.contents):
+            if raw_word:
+                word = correct_word(raw_word)
+                if word != raw_word:
+                    self.contents = self.contents.replace(raw_word, word)
+                    self.report['{} -> {}'.format(raw_word, word)] += 1
 
-    def get_word(self, index):
-        beg = fin = None
-        delta = 0
-        while True:
-            if None not in (beg, fin):
-                return self.contents[beg: fin]
-            delta += 1
-            if self.contents[index - delta] in " \n\t\r\f\v":
-                fin = index + delta
-            if self.contents[index + delta] in " \n\t\r\f\v":
-                fin = index + delta
-
-    def is_beg_or_fin(self, index):
-        try:
-            char = self.contents[index]
-        except IndexError:
-            return True
-        if char in " \n\t\r\f\v":
-            pass
+    def deliver_report(self):
+        print("{} separate instances, {} fixes".format(len(self.report), sum(self.report.values())))
+        if self.report:
+            if self.dump:
+                with open(self.dump, 'w', encoding='utf-8') as handler:
+                    json.dump(self.report, handler, ensure_ascii=False, sort_keys=True, indent=2)
+            else:
+                for fix in self.report:
+                    print(fix)
 
 
+def correct_word(raw_word):
+    word = re.sub(r'(и)(?=[аеёиоуыэюя])', i_fixer, raw_word, flags=re.IGNORECASE)
+    if word[-1] in 'бвгджзклмнпрстфхцчщщ':
+        word += 'ъ'
+    return word
 
+
+def i_fixer(matchobj):
+    if matchobj.group(1).isupper():
+        return 'І'
+    else:
+        return 'і'
 
 
 if __name__ == '__main__':
