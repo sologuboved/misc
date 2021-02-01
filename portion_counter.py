7eb6fcecf3a2c0e7827e9412d006dd7dc075e3bc
sologuboved <sologuboved@gmail.com>
2021-02-01 14:17:00 +0300

    upd

--- portion_counter.py
+++ portion_counter.py
@@ -2,7 +2,7 @@ from datetime import date
 
 
 def count_portion(beg_date, fin_date, beg_item, fin_item, per_day=5, initial_amount=None):
-    res = (fin_item - beg_item - (fin_date - beg_date).days * per_day) / per_day
+    res = (fin_item - beg_item - ((fin_date - beg_date).days + 1) * per_day) / per_day
     if initial_amount:
         print(initial_amount + res)
     else:
@@ -10,4 +10,4 @@ def count_portion(beg_date, fin_date, beg_item, fin_item, per_day=5, initial_amo
 
 
 if __name__ == '__main__':
-    count_portion(date(2020, 9, 3), date(2020, 9, 8), 250, 490, initial_amount=337)
+    count_portion(date(2021, 1, 25), date(2021, 2, 1), 1, 100, initial_amount=193)
